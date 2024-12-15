c5226f9beb2242fca059fbd480ba251a9e83f522
yulrio <33221012@std.stei.itb.ac.id>
2024-12-15 09:50:57 +0700

    initial commit

--- ckanext/harvestapi/plugin.py
+++ ckanext/harvestapi/plugin.py
@@ -1,14 +1,33 @@
+import jwt
+import requests
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 
 class HarvestapiPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IBlueprint)
 
     # IConfigurer
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
-            'harvestapi')
+        toolkit.add_resource('fanstatic','harvestapi')
+
+    # IBlueprint
+    def get_blueprint(self):
+        # Method untuk mendaftarkan Blueprint.
+        blueprint_harvestapi = Blueprint('harvestapi', __name__,url_prefix='/api/1/harvest')
+
+        @blueprint_harvestapi.route('/welcome-harvest', methods=['GET'])
+        def welcome_api():
+            """
+            Route untuk /welcome_harvest
+            """
+            return jsonify({
+                "message": "Welcome to Harvest API",
+                "success": True
+            })
+
+        return blueprint_harvestapi
---
+++ update_harvestapi.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Pesan commit untuk git
+COMMIT_MESSAGE="Initial Commit"
+
+# Perintah git di host
+cd "$HOST_EXT_PATH" || exit
+git add .
+git commit -m "$COMMIT_MESSAGE"
+git push -u origin master
+
+# Nama container Docker
+CONTAINER_NAME="ckan"
+
+# Path ke direktori ekstensi di dalam container
+EXT_PATH="/srv/app/ext_2024/ckanext-harvestapi"
+
+# Perintah untuk menjalankan pembaruan
+docker exec "$CONTAINER_NAME" bash -c "
+    cd $EXT_PATH &&
+    git pull &&
+    pip install -e .
+"
+
+# Restart container (opsional, hapus jika tidak diperlukan)
+docker restart "$CONTAINER_NAME"
