dd52e258d1fe21fc227354ad534a592aebc969f5
yulrio <33221012@std.stei.itb.ac.id>
2024-12-16 13:50:03 +0700

    Initial Commit

--- ckanext/harvestapi/plugin.py
+++ ckanext/harvestapi/plugin.py
@@ -42,23 +42,66 @@ class HarvestapiPlugin(plugins.SingletonPlugin):
             """
             try:
                 # Query data dari tabel HarvestObject (atau sesuai kebutuhan Anda)
+                payload = request.get_json()
                 token = request.headers.get("Authorization")
-                preferred_username, email = get_username(token)
+                if not payload:
+                    return jsonify({"success": False, "error": "Request body is required"}), 400
+
+                token = request.headers.get("Authorization")
+                _, email = get_username(token)
                 username = email.split('@')[0]
+                
+                # Ambil parameter dari payload JSON
+                query = payload.get('q', '').strip()
+                rows = int(payload.get('rows', 10))
+                start = int(payload.get('start', 0))
+                sort = payload.get('sort', 'prioritas_tahun desc')
+                facet_limit = int(payload.get('facet.limit', 500))
+                organization = payload.get('organization', '').strip()
+                kategori = payload.get('kategori', '').strip()
+                prioritas_tahun = payload.get('prioritas_tahun', '').strip()
+                tags = payload.get('tags', '').strip()
+                res_format = payload.get('res_format', '').strip()
 
-                params = {'limit': 10, 'offset': 0} 
-                context = {"user": username}
-                print(context,username)
+                # Periksa panjang query
+                if len(query) == 0:  # Jika panjang query 0
+                    query = '*:*'
+                elif query != '*:*':  # Jika query bukan '*:*', gunakan format pencarian
+                    query = f"(title:*{query}* OR notes:*{query}*)"
+                
+                if organization:
+                    query += f" AND organization:{organization}"
+                if kategori:
+                    query += f" AND kategori:{kategori}"
+                if prioritas_tahun:
+                    query += f" AND prioritas_tahun:{prioritas_tahun}"
+                if tags:
+                    query += f" AND tags:{tags}"
+                if res_format:
+                    query += f" AND res_format:{res_format}"
+                
+                # Parameter untuk Solr
+                params = {
+                    'q': query,  # Query utama
+                    'wt': 'json',
+                    'rows': rows,
+                    'start': start,
+                    'sort': sort,
+                    'facet': 'true',
+                    'facet.field': ['organization', 'kategori', 'prioritas_tahun', 'tags', 'res_format'],
+                    'facet.limit': facet_limit
+                }
 
-                harvest_objects = toolkit.get_action('package_activity_list')(
-                    # context={"user":username}, 
-                    data_dict={'limit': 10, 'offset': 0,'id':'a16b19f3-4883-4a13-8846-5a60c82313b6'}
-                )
+                context = {'user': username,'ignore_auth': True}
+
+                # Jalankan package_search
+                response = get_action('package_search')(context, params)
+
 
                 # Kembalikan data dalam format JSON
                 return jsonify({
                     "success": True,
-                    "result": harvest_objects
+                    "result": response
                 })
             except Exception as e:
                 # Tangani error dan kembalikan pesan
