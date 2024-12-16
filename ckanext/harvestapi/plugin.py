fd13f84eaba0fcd6849a3637f7e84c73fc822789
yulrio <33221012@std.stei.itb.ac.id>
2024-12-16 16:14:49 +0700

    Initial Commit

--- ckanext/harvestapi/plugin.py
+++ ckanext/harvestapi/plugin.py
@@ -106,7 +106,7 @@ class HarvestapiPlugin(plugins.SingletonPlugin):
                 package_detail = get_package_detail(harvest_source_id)
                 owner_org = package_detail["owner_org"]
                 username_capacity = get_organization_admin(username,owner_org)
-                print(username_capacity)
+                print("username_capacity: "username_capacity)
 
                 # Parameter untuk Solr
                 params = {
